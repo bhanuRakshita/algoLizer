ac5b6fb8a9eec8f36be7019d19bc0c921050b9fa
Bhanu Rakshita Paul <bhanurakshita457@gmail.com>
2023-01-06 17:13:33 -0500

    debug selection, insertion and bubble sorts

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use('/css', express.static(__dirname+'/css'));
+app.use('/js', express.static(__dirname+'/js'));
+app.use('/src', express.static(__dirname+'/src'));
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/loadingPage.html");
+});
+
